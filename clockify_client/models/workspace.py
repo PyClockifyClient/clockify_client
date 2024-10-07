990db762dfb4df03fecd671ed2f59bcf8c56aa1c
Marcel Wilson <m.wilson@acustaf.com>
2024-10-07 09:09:21 -0500

    adding `add_user`

--- clockify_client/models/workspace.py
+++ clockify_client/models/workspace.py
@@ -18,3 +18,12 @@ class Workspace(AbstractClockify):
         path = "/workspaces/"
 
         return self.get(path)
+
+    def get_workspace_info(self, workspace_id: str) -> JsonType:
+        """Returns all workspaces.
+
+        https://docs.clockify.me/#tag/Workspace/operation/getWorkspacesOfUser
+        """
+        path = f"/workspaces/{workspace_id}"
+
+        return self.get(path)
